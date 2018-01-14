6a697fffaa235607da9d31253fdaa7ac9093d34f
Tom Woodward <twwoodward@me.com>
2018-01-13 20:13:36 -0500

    accessibility changes

--- js/rvarts-home.js
+++ js/rvarts-home.js
@@ -25,7 +25,7 @@ var match =  classDate(data);
       '<div id="' +
       data.id +
       '" class="event '+classDate(data)+ '"' +
-      '><a href="'+data.website+'"><div class="event-bg h-100"><div class="card event-content h-100"><img src="'+featuredImg(data)+'"><h2 class="card-title event-title">' +
+      '><a href="'+data.website+'"><div class="event-bg h-100"><div class="card event-content h-100"><img src="'+featuredImg(data)+'" alt="'+data.title+'"><h2 class="card-title event-title">' +
       data.title +
       '</h2>'+theDate(data)+'</div></div></a></div>' 
     ) 
--- page-templates/fullwidthpage-home.php
+++ page-templates/fullwidthpage-home.php
@@ -10,17 +10,16 @@
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
-
+<h1 id="events">Upcoming Events</h1>
 <div class="wrapper" id="date-wrapper">
-
-	<div class="container">
+	<div class="container" id="content">
           <div class="row" id="dayContent">
           </div>        
           <div class="row" id="monthContent">
           </div>
           <div class="row" id="otherContent">
           </div>
-        </div>
+    </div>
 
 </div><!-- Wrapper end -->
 
